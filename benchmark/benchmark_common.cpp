ac7b2a5a79783ff259108514059ff595dd0d4f67
esrrhs <esrrhs@163.com>
2022-07-01 18:29:19 +0800

    fix build

--- benchmark/benchmark_common.cpp
+++ benchmark/benchmark_common.cpp
@@ -1,9 +1,11 @@
 #include "benchmark/benchmark.h"
 #include "fakelua/fakelua.h"
+#include "glog/logging.h"
 
 using namespace fakelua;
 
 static void BM_ini(benchmark::State &state) {
+    FLAGS_minloglevel = 3;
     for (auto _: state) {
         for (int i = 0; i < state.range(0); ++i) {
             auto L = fakelua_newstate();
