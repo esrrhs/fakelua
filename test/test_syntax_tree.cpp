213e8f6b03d178b5a7818df705252559c1299711
esrrhs <esrrhs@163.com>
2023-03-19 22:13:45 +0800

    add test case

--- test/test_syntax_tree.cpp
+++ test/test_syntax_tree.cpp
@@ -21,3 +21,21 @@ TEST(syntax_tree, label) {
 
     ASSERT_EQ(dumpstr, wantstr);
 }
+
+TEST(syntax_tree, assign_simple) {
+    auto L = fakelua_newstate();
+    ASSERT_NE(L.get(), nullptr);
+
+    compiler c;
+    auto result = c.compile_file("./test_assign_simple.lua");
+    ASSERT_NE(result.chunk, nullptr);
+
+    auto dumpstr = result.chunk->dump();
+    LOG(INFO) << "\n" << dumpstr;
+
+    auto wantstr = "(block)[2:1]\n"
+                   "  aa(label)[2:3]\n"
+                   "  bb(label)[4:3]\n";
+
+    ASSERT_EQ(dumpstr, wantstr);
+}
