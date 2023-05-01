22d65e583e90ac66577cd11dc9294bd1fcdd125a
esrrhs <esrrhs@163.com>
2023-05-01 11:42:33 +0800

    add test

---
+++ test/lua/interpreter/test_const_define.lua
@@ -0,0 +1,10 @@
+local a
+local b = 1
+local c <const> = {
+    d = true,
+    e = "2",
+    f = {
+        g = 1.23,
+        h = { 1001, 1002, 1003 }
+    }
+}
---
+++ test/test_interpreter.cpp
@@ -0,0 +1,13 @@
+#include "compile/compiler.h"
+#include "fakelua.h"
+#include "gtest/gtest.h"
+
+using namespace fakelua;
+
+TEST(interpreter, const_define) {
+    auto L = fakelua_newstate();
+    ASSERT_NE(L.get(), nullptr);
+
+    compiler c;
+    auto result = c.compile_file("./interpreter/test_const_define.lua", {});
+}
