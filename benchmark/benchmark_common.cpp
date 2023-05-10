34b52f871dc18549656af94c97f6df10eefd3d9f
esrrhs <esrrhs@163.com>
2023-05-10 12:32:30 +0800

    add bench

--- benchmark/benchmark_common.cpp
+++ benchmark/benchmark_common.cpp
@@ -2,6 +2,7 @@
 #include "fakelua.h"
 #include "util/common.h"
 #include "util/concurrent_hashmap.h"
+#include "util/concurrent_vector.h"
 
 using namespace fakelua;
 
@@ -28,3 +29,32 @@ static void BM_normal_hashmap(benchmark::State &state) {
 }
 
 BENCHMARK(BM_normal_hashmap)->Range(8, 8 << 10);
+
+static void BM_concurrent_vector(benchmark::State &state) {
+    FLAGS_minloglevel = 3;
+    concurrent_vector<std::string> v(STRING_HEAP_INIT_BUCKET_SIZE);
+    for (auto _: state) {
+        for (int i = 0; i < state.range(0); ++i) {
+            for (int j = 0; j < 100; ++j) {
+                v.set(j, "hello");
+            }
+        }
+    }
+}
+
+BENCHMARK(BM_concurrent_vector)->Range(8, 8 << 10);
+
+static void BM_normal_vector(benchmark::State &state) {
+    FLAGS_minloglevel = 3;
+    std::vector<std::string> v;
+    v.resize(STRING_HEAP_INIT_BUCKET_SIZE);
+    for (auto _: state) {
+        for (int i = 0; i < state.range(0); ++i) {
+            for (int j = 0; j < 100; ++j) {
+                v[j] = "hello";
+            }
+        }
+    }
+}
+
+BENCHMARK(BM_normal_vector)->Range(8, 8 << 10);
