917a81fe270ee963fb72e0cc9f838509c083f8ce
esrrhs <esrrhs@163.com>
2023-12-17 15:18:54 +0800

    add test case

--- src/util/file_util.cpp
+++ src/util/file_util.cpp
@@ -6,6 +6,11 @@ namespace fakelua {
 std::string generate_tmp_filename(const std::string &head, const std::string &tail) {
     auto tmpdir = std::filesystem::temp_directory_path();
     tmpdir += std::filesystem::path::preferred_separator;
+    tmpdir += "fakelua";
+    tmpdir += std::filesystem::path::preferred_separator;
+    if (!std::filesystem::exists(tmpdir)) {
+        std::filesystem::create_directories(tmpdir);
+    }
     tmpdir += head;
     // create tmp file in system temp dir
     std::string fileName;
--- test/test_jitter.cpp
+++ test/test_jitter.cpp
@@ -215,3 +215,25 @@ TEST(jitter, multi_const_define) {
     ASSERT_EQ(ret4, "test");
     ASSERT_EQ(std::abs(ret5 - 2.3) < 0.001, true);
 }
+
+TEST(jitter, empty_func_with_params) {
+    auto L = fakelua_newstate();
+    ASSERT_NE(L.get(), nullptr);
+
+    int ret1 = 0;
+    bool ret2 = false;
+    std::string ret3;
+    double ret4;
+    L->compile_file("./jit/test_empty_func_with_params.lua", {});
+    L->call("test", std::tie(ret1, ret2, ret3, ret4), 2.3, "test", true, 1);
+    ASSERT_EQ(ret1, 1);
+    ASSERT_EQ(ret2, true);
+    ASSERT_EQ(ret3, "test");
+    ASSERT_EQ(std::abs(ret4 - 2.3) < 0.001, true);
+    L->compile_file("./jit/test_empty_func_with_params.lua", {debug_mode: false});
+    L->call("test", std::tie(ret1, ret2, ret3, ret4), 2.3, "test", true, 1);
+    ASSERT_EQ(ret1, 1);
+    ASSERT_EQ(ret2, true);
+    ASSERT_EQ(ret3, "test");
+    ASSERT_EQ(std::abs(ret4 - 2.3) < 0.001, true);
+}
