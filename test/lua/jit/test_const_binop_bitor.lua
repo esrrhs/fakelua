b06df2b682d5b85ec80acb32a585240eb82a8d27
esrrhs <esrrhs@163.com>
2024-01-04 19:41:21 +0800

    add test case

---
+++ test/lua/jit/test_const_binop_bitor.lua
@@ -0,0 +1,9 @@
+local a = 2
+local b = 3
+local c = a | b
+local d = "4"
+local e = 22
+local f = d | e
+function test()
+    return c, f
+end
--- test/test_jitter.cpp
+++ test/test_jitter.cpp
@@ -1491,3 +1491,41 @@ TEST(jitter, test_const_binop_xor) {
     ASSERT_EQ(ret1, 1);
     ASSERT_EQ(ret2, 18);
 }
+
+TEST(jitter, test_binop_bitor) {
+    auto L = fakelua_newstate();
+    ASSERT_NE(L.get(), nullptr);
+
+    int ret1 = 0;
+    int ret2 = 0;
+    L->compile_file("./jit/test_binop_bitor.lua", {});
+    L->call("test", std::tie(ret1, ret2), 3, 2, "4", 12);
+    ASSERT_EQ(ret1, 7);
+    ASSERT_EQ(ret2, 15);
+
+    ret1 = 0;
+    ret2 = 0;
+    L->compile_file("./jit/test_binop_bitor.lua", {debug_mode: false});
+    L->call("test", std::tie(ret1, ret2), 3, 2, "4", 12);
+    ASSERT_EQ(ret1, 7);
+    ASSERT_EQ(ret2, 15);
+}
+
+TEST(jitter, test_const_binop_bitor) {
+    auto L = fakelua_newstate();
+    ASSERT_NE(L.get(), nullptr);
+
+    int ret1 = 0;
+    int ret2 = 0;
+    L->compile_file("./jit/test_const_binop_bitor.lua", {});
+    L->call("test", std::tie(ret1, ret2));
+    ASSERT_EQ(ret1, 3);
+    ASSERT_EQ(ret2, 22);
+
+    ret1 = 0;
+    ret2 = 0;
+    L->compile_file("./jit/test_const_binop_bitor.lua", {debug_mode: false});
+    L->call("test", std::tie(ret1, ret2));
+    ASSERT_EQ(ret1, 3);
+    ASSERT_EQ(ret2, 22);
+}
