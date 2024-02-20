50e774b8df2911930e8cf03d537b152ea9261f17
esrrhs <esrrhs@163.com>
2024-02-20 17:35:33 +0800

    no, just change map value from var* to container*, to make var * a fixed memory. so we can use var** in assign function, and just copy var* value. that is fast and simple.

--- src/jit/vm.cpp
+++ src/jit/vm.cpp
@@ -889,7 +889,6 @@ extern "C" __attribute__((used)) var *call_var(fakelua_state *s, var *func, int 
     DEBUG_ASSERT(n >= 0);
 
     // TODO
-
     return nullptr;
 }
 
@@ -901,7 +900,6 @@ extern "C" __attribute__((used)) var *table_index_var(fakelua_state *s, var *tab
     DEBUG_ASSERT(key->type() >= var_type::VAR_MIN && key->type() <= var_type::VAR_MAX);
 
     // TODO
-
     return nullptr;
 }
 
