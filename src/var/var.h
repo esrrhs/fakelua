206bc826df46966cf14bfb07d35e611e8f4fc2e0
esrrhs <esrrhs@163.com>
2025-05-25 12:54:28 +0800

    next: 1. remove const define 2. change var define and var table define 3. use local var in gccjit to speedup 4. move binop in gccjit to speedup

--- src/var/var.h
+++ src/var/var.h
@@ -246,15 +246,16 @@ public:
 private:
     // use class members instead of union, use more memory but more safe and fast.
     int type_ = var_type::VAR_NIL;
-    bool is_const_ = false;
-    bool is_variadic_ = false;
     union data_ {
         bool b;
         int64_t i;
         double f;
         const char *s;
-        void* t;
+        void *t;
     };
+    data_ data_{};
+    bool is_const_ = false;
+    bool is_variadic_ = false;
     bool bool_ = false;
     int64_t int_ = 0;
     double float_ = 0;
