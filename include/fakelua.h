89eef6187caf04c4aa9eb4be75321f0899264c50
esrrhs <esrrhs@163.com>
2026-03-22 12:24:33 +0800

    update

--- include/fakelua.h
+++ include/fakelua.h
@@ -8,9 +8,7 @@
 
 namespace fakelua {
 
-// use to describe the complex type of lua.
-// communication between fakelua and native.
-// so it can transfer complex type like table.
+// 用于FakeLua与原生代码之间的通信，传输像table这样的复杂类型。
 struct VarInterface {
     enum class Type {
         MIN,
@@ -54,7 +52,7 @@ struct VarInterface {
     [[nodiscard]] virtual std::string ViToString(int tab) const = 0;
 };
 
-// simple var implement, just for simple use.
+// 简单的实现，仅用于测试和调试。实际使用中，用户可以根据需要实现自己的 VarInterface。
 struct SimpleVarImpl final : public VarInterface {
     SimpleVarImpl() = default;
 
@@ -137,9 +135,8 @@ struct SimpleVarImpl final : public VarInterface {
                 break;
             case Type::TABLE:
                 ret = "table:";
-                for (auto &kv: table_) {
-                    ret += std::format("\n{}[{}] = {}", std::string(tab + 1, '\t'), kv.first->ViToString(tab + 1),
-                                       kv.second->ViToString(tab + 1));
+                for (const auto &[fst, snd]: table_) {
+                    ret += std::format("\n{}[{}] = {}", std::string(tab + 1, '\t'), fst->ViToString(tab + 1), snd->ViToString(tab + 1));
                 }
                 break;
         }
@@ -147,7 +144,7 @@ struct SimpleVarImpl final : public VarInterface {
         return ret;
     }
 
-    // sort table by key, just for debug
+    // 按键对表进行排序，仅用于调试
     void ViSortTable() {
         std::sort(table_.begin(), table_.end(), [](const auto &a, const auto &b) {
             if (a.first->ViGetType() != b.first->ViGetType()) {
@@ -189,6 +186,7 @@ class VarTable;
 class VarString;
 class Var;
 
+// 基本变量类型，包含类型标识和数据。数据使用 union 存储，根据类型标识来访问。
 struct CVar {
 protected:
     int type_ = 0;
@@ -203,189 +201,177 @@ protected:
     cvar_data data_{};
 };
 
-// control the Compiler behavior
+// 控制编译器的配置项
 struct CompileConfig {
-    // skip jit compile. just lex and parse.
+    // 跳过 JIT 编译。仅进行词法分析和语法解析。
     bool skip_jit = false;
-    // debug mode. if true, the jit code will be dumped to file.
+    // 调试模式。如果为 true，JIT 代码将被转储到文件中。
     bool debug_mode = true;
 };
 
 struct StateConfig {
-    // max var stack count
+    // 最大栈深，超过该深度将抛出异常。默认为 65536。
     size_t max_stack_size = 65536;
 };
 
-// fake_lua state interface, every state can only run in one thread, just like Lua.
-// every state has its own running environment. there could be many states in one process.
-class FakeluaState : public std::enable_shared_from_this<FakeluaState> {
-public:
-    explicit FakeluaState(StateConfig config = {}) : config_(config) {
-    }
+class State;
 
-    virtual ~FakeluaState() = default;
+// 创建 FakeLua 状态
+State *FakeluaNewState(const StateConfig &cfg = {});
 
-    // compile file, the file is a lua file.
-    virtual void CompileFile(const std::string &filename, const CompileConfig &cfg) = 0;
+// 释放 FakeLua 状态
+void FakeluaFreeState(State *s);
 
-    // compile string, the string is the content of a file.
-    virtual void CompileString(const std::string &str, const CompileConfig &cfg) = 0;
+// 编译文件
+void CompileFile(State *s, const std::string &filename, const CompileConfig &cfg);
 
-    // call function by name
-    template<typename... Rets, typename... Args>
-    void call(const std::string &name, std::tuple<Rets &...> &&rets, Args &&...args);
+// 编译字符串
+void CompileString(State *s, const std::string &str, const CompileConfig &cfg);
 
-    // set VarInterface new instance function
-    void SetVarInterfaceNewFunc(const std::function<VarInterface *()> &func) {
-        var_interface_new_func_ = func;
-    }
+// 调用某个Lua里的函数
+template<typename... Rets, typename... Args>
+void Call(State *s, const std::string &name, std::tuple<Rets &...> &&rets, Args &&...args);
 
-    // get VarInterface new instance function
-    std::function<VarInterface *()> &GetVarInterfaceNewFunc() {
-        return var_interface_new_func_;
-    }
+// 设置 VarInterface 构造实例函数
+void SetVarInterfaceNewFunc(State *s, const std::function<VarInterface *()> &func);
 
-    // set global debug log level, note all state will be set.
-    // 0: off, 1: error, 2: info, default is error.
-    void SetDebugLogLevel(int level);
+// 获取 VarInterface 构造实例函数
+std::function<VarInterface *()> &GetVarInterfaceNewFunc(State *s);
 
-private:
-    std::function<VarInterface *()> var_interface_new_func_;
-    StateConfig config_;
-};
-
-using FakeluaStatePtr = std::shared_ptr<FakeluaState>;
+// 设置全局调试日志级别，注意所有状态都将被设置。
+// 0: 关闭, 1: 错误, 2: 信息, 默认为错误。
+void SetDebugLogLevel(State *s, int level);
 
 namespace inter {
 
-// native to fakelua
-CVar NativeToFakeluaNil(const FakeluaStatePtr &s);
-CVar NativeToFakeluaBool(const FakeluaStatePtr &s, bool v);
-CVar NativeToFakeluaChar(const FakeluaStatePtr &s, char v);
-CVar NativeToFakeluaUchar(const FakeluaStatePtr &s, unsigned char v);
-CVar NativeToFakeluaShort(const FakeluaStatePtr &s, short v);
-CVar NativeToFakeluaUshort(const FakeluaStatePtr &s, unsigned short v);
-CVar NativeToFakeluaInt(const FakeluaStatePtr &s, int v);
-CVar NativeToFakeluaUint(const FakeluaStatePtr &s, unsigned int v);
-CVar NativeToFakeluaLong(const FakeluaStatePtr &s, long v);
-CVar NativeToFakeluaUlong(const FakeluaStatePtr &s, unsigned long v);
-CVar NativeToFakeluaLonglong(const FakeluaStatePtr &s, long long v);
-CVar NativeToFakeluaUlonglong(const FakeluaStatePtr &s, unsigned long long v);
-CVar NativeToFakeluaFloat(const FakeluaStatePtr &s, float v);
-CVar NativeToFakeluaDouble(const FakeluaStatePtr &s, double v);
-CVar NativeToFakeluaCstr(const FakeluaStatePtr &s, const char *v);
-CVar NativeToFakeluaStr(const FakeluaStatePtr &s, char *v);
-CVar NativeToFakeluaString(const FakeluaStatePtr &s, const std::string &v);
-CVar NativeToFakeluaStringview(const FakeluaStatePtr &s, const std::string_view &v);
-CVar NativeToFakeluaObj(const FakeluaStatePtr &s, const VarInterface *v);
+// 原生转 FakeLua
+CVar NativeToFakeluaNil(State *s);
+CVar NativeToFakeluaBool(State *s, bool v);
+CVar NativeToFakeluaChar(State *s, char v);
+CVar NativeToFakeluaUchar(State *s, unsigned char v);
+CVar NativeToFakeluaShort(State *s, short v);
+CVar NativeToFakeluaUshort(State *s, unsigned short v);
+CVar NativeToFakeluaInt(State *s, int v);
+CVar NativeToFakeluaUint(State *s, unsigned int v);
+CVar NativeToFakeluaLong(State *s, long v);
+CVar NativeToFakeluaUlong(State *s, unsigned long v);
+CVar NativeToFakeluaLonglong(State *s, long long v);
+CVar NativeToFakeluaUlonglong(State *s, unsigned long long v);
+CVar NativeToFakeluaFloat(State *s, float v);
+CVar NativeToFakeluaDouble(State *s, double v);
+CVar NativeToFakeluaCstr(State *s, const char *v);
+CVar NativeToFakeluaStr(State *s, char *v);
+CVar NativeToFakeluaString(State *s, const std::string &v);
+CVar NativeToFakeluaStringview(State *s, const std::string_view &v);
+CVar NativeToFakeluaObj(State *s, const VarInterface *v);
 
 template<typename T>
-CVar NativeToFakelua(const FakeluaStatePtr &s, T v) {
-    // check if T is nil
+CVar NativeToFakelua(State *s, T v) {
+    // 检查 T 是否为 nil
     if constexpr (std::is_same_v<T, std::nullptr_t>) {
         return NativeToFakeluaNil(s);
     }
-    // check if T is bool
+    // 检查 T 是否为 bool
     else if constexpr (std::is_same_v<T, bool>) {
         return NativeToFakeluaBool(s, v);
     }
-    // check if T is char
+    // 检查 T 是否为 char
     else if constexpr (std::is_same_v<T, char>) {
         return NativeToFakeluaChar(s, v);
     }
-    // check if T is unsigned char
+    // 检查 T 是否为 unsigned char
     else if constexpr (std::is_same_v<T, unsigned char>) {
         return NativeToFakeluaUchar(s, v);
     }
-    // check if T is short
+    // 检查 T 是否为 short
     else if constexpr (std::is_same_v<T, short>) {
         return NativeToFakeluaShort(s, v);
     }
-    // check if T is unsigned short
+    // 检查 T 是否为 unsigned short
     else if constexpr (std::is_same_v<T, unsigned short>) {
         return NativeToFakeluaUshort(s, v);
     }
-    // check if T is int
+    // 检查 T 是否为 int
     else if constexpr (std::is_same_v<T, int>) {
         return NativeToFakeluaInt(s, v);
     }
-    // check if T is unsigned int
+    // 检查 T 是否为 unsigned int
     else if constexpr (std::is_same_v<T, unsigned int>) {
         return NativeToFakeluaUint(s, v);
     }
-    // check if T is long
+    // 检查 T 是否为 long
     else if constexpr (std::is_same_v<T, long>) {
         return NativeToFakeluaLong(s, v);
     }
-    // check if T is unsigned long
+    // 检查 T 是否为 unsigned long
     else if constexpr (std::is_same_v<T, unsigned long>) {
         return NativeToFakeluaUlong(s, v);
     }
-    // check if T is long long
+    // 检查 T 是否为 long long
     else if constexpr (std::is_same_v<T, long long>) {
         return NativeToFakeluaLonglong(s, v);
     }
-    // check if T is unsigned long long
+    // 检查 T 是否为 unsigned long long
     else if constexpr (std::is_same_v<T, unsigned long long>) {
         return NativeToFakeluaUlonglong(s, v);
     }
-    // check if T is float
+    // 检查 T 是否为 float
     else if constexpr (std::is_same_v<T, float>) {
         return NativeToFakeluaFloat(s, v);
     }
-    // check if T is double
+    // 检查 T 是否为 double
     else if constexpr (std::is_same_v<T, double>) {
         return NativeToFakeluaDouble(s, v);
     }
-    // check if T is const char *
+    // 检查 T 是否为 const char *
     else if constexpr (std::is_same_v<T, const char *>) {
         return NativeToFakeluaCstr(s, v);
     }
-    // check if T is char *
+    // 检查 T 是否为 char *
     else if constexpr (std::is_same_v<T, char *>) {
         return NativeToFakeluaStr(s, v);
     }
-    // check if T is std::string
+    // 检查 T 是否为 std::string
     else if constexpr (std::is_same_v<T, std::string>) {
         return NativeToFakeluaString(s, v);
     }
-    // check if T is std::string_view
+    // 检查 T 是否为 std::string_view
     else if constexpr (std::is_same_v<T, std::string_view>) {
         return NativeToFakeluaStringview(s, v);
     }
-    // check if T is a cvar
+    // 检查 T 是否为 cvar
     else if constexpr (std::is_same_v<T, CVar>) {
         return v;
     } else {
-        // static_assert T should be VarInterface* or implement VarInterface
+        // 静态断言 T 应该是 VarInterface* 或实现 VarInterface
         static_assert(std::is_pointer_v<T>, "T should be pointer");
         static_assert(std::is_base_of_v<VarInterface, std::remove_pointer_t<T>>, "T should be VarInterface");
         return NativeToFakeluaObj(s, v);
     }
 }
 
-// fakelua to native
-bool FakeluaToNativeBool(const FakeluaStatePtr &s, CVar v);
-char FakeluaToNativeChar(const FakeluaStatePtr &s, CVar v);
-unsigned char FakeluaToNativeUchar(const FakeluaStatePtr &s, CVar v);
-short FakeluaToNativeShort(const FakeluaStatePtr &s, CVar v);
-unsigned short FakeluaToNativeUshort(const FakeluaStatePtr &s, CVar v);
-int FakeluaToNativeInt(const FakeluaStatePtr &s, CVar v);
-unsigned int FakeluaToNativeUint(const FakeluaStatePtr &s, CVar v);
-long FakeluaToNativeLong(const FakeluaStatePtr &s, CVar v);
-unsigned long FakeluaToNativeUlong(const FakeluaStatePtr &s, CVar v);
-long long FakeluaToNativeLonglong(const FakeluaStatePtr &s, CVar v);
-unsigned long long FakeluaToNativeUlonglong(const FakeluaStatePtr &s, CVar v);
-float FakeluaToNativeFloat(const FakeluaStatePtr &s, CVar v);
-double FakeluaToNativeDouble(const FakeluaStatePtr &s, CVar v);
-const char *FakeluaToNativeCstr(const FakeluaStatePtr &s, CVar v);
-const char *FakeluaToNativeStr(const FakeluaStatePtr &s, CVar v);
-std::string FakeluaToNativeString(const FakeluaStatePtr &s, CVar v);
-std::string_view FakeluaToNativeStringview(const FakeluaStatePtr &s, CVar v);
-VarInterface *FakeluaToNativeObj(const FakeluaStatePtr &s, CVar v);
+// FakeLua 转原生
+bool FakeluaToNativeBool(State *s, CVar v);
+char FakeluaToNativeChar(State *s, CVar v);
+unsigned char FakeluaToNativeUchar(State *s, CVar v);
+short FakeluaToNativeShort(State *s, CVar v);
+unsigned short FakeluaToNativeUshort(State *s, CVar v);
+int FakeluaToNativeInt(State *s, CVar v);
+unsigned int FakeluaToNativeUint(State *s, CVar v);
+long FakeluaToNativeLong(State *s, CVar v);
+unsigned long FakeluaToNativeUlong(State *s, CVar v);
+long long FakeluaToNativeLonglong(State *s, CVar v);
+unsigned long long FakeluaToNativeUlonglong(State *s, CVar v);
+float FakeluaToNativeFloat(State *s, CVar v);
+double FakeluaToNativeDouble(State *s, CVar v);
+const char *FakeluaToNativeCstr(State *s, CVar v);
+const char *FakeluaToNativeStr(State *s, CVar v);
+std::string FakeluaToNativeString(State *s, CVar v);
+std::string_view FakeluaToNativeStringview(State *s, CVar v);
+VarInterface *FakeluaToNativeObj(State *s, CVar v);
 
 template<typename T>
-T FakeluaToNative(const FakeluaStatePtr &s, const CVar v) {
+T FakeluaToNative(State *s, const CVar v) {
     if constexpr (std::is_same_v<T, bool>) {
         return FakeluaToNativeBool(s, v);
     } else if constexpr (std::is_same_v<T, char>) {
@@ -423,7 +409,7 @@ T FakeluaToNative(const FakeluaStatePtr &s, const CVar v) {
     } else if constexpr (std::is_same_v<T, CVar>) {
         return v;
     } else {
-        // static_assert T should be VarInterface* or implement VarInterface
+        // 静态断言 T 应该是 VarInterface* 或实现 VarInterface
         static_assert(std::is_pointer_v<T>, "T should be pointer");
         static_assert(std::is_base_of_v<VarInterface, std::remove_pointer_t<T>>, "T should be VarInterface");
         return FakeluaToNativeObj(s, v);
@@ -431,38 +417,34 @@ T FakeluaToNative(const FakeluaStatePtr &s, const CVar v) {
 }
 
 template<size_t I = 0, typename... Rets>
-inline std::enable_if_t<I == sizeof...(Rets), void> FakeluaFuncRetHelper(const FakeluaStatePtr &s, CVar *ret, std::tuple<Rets &...> &rets) {
+inline std::enable_if_t<I == sizeof...(Rets), void> FakeluaFuncRetHelper(State *s, CVar *ret, std::tuple<Rets &...> &rets) {
 }
 
 template<size_t I = 0, typename... Rets>
-        inline std::enable_if_t <
-        I<sizeof...(Rets), void> FakeluaFuncRetHelper(const FakeluaStatePtr &s, CVar *ret, std::tuple<Rets &...> &rets) {
+        inline std::enable_if_t < I<sizeof...(Rets), void> FakeluaFuncRetHelper(State *s, CVar *ret, std::tuple<Rets &...> &rets) {
     typedef std::remove_reference_t<std::tuple_element_t<I, std::tuple<Rets &...>>> t;
     std::get<I>(rets) = FakeluaToNative<t>(s, ret[I]);
     FakeluaFuncRetHelper<I + 1, Rets...>(s, ret, rets);
 }
 
-void call(const FakeluaStatePtr &s, const std::string &name, CVar *args, size_t arg_size, CVar *rets, size_t ret_size);
+void Call(State *s, const std::string &name, CVar *args, size_t arg_size, CVar *rets, size_t ret_size);
 
 }// namespace inter
 
-// call funtion by name
+// 调用函数
 template<typename... Rets, typename... Args>
-void FakeluaState::call(const std::string &name, std::tuple<Rets &...> &&rets, Args &&...args) {
-    // transfer args to vars array
-    CVar args_array[sizeof...(Args)] = {inter::NativeToFakelua(shared_from_this(), std::forward<Args>(args))...};
+void Call(State *s, const std::string &name, std::tuple<Rets &...> &&rets, Args &&...args) {
+    // 将参数传输到变量数组
+    CVar args_array[sizeof...(Args)] = {inter::NativeToFakelua(s, std::forward<Args>(args))...};
 
-    // alloc vars in stack
+    // 在栈中分配变量
     CVar rets_array[sizeof...(Rets)] = {};
 
-    // call function
-    inter::call(shared_from_this(), name, args_array, sizeof...(Args), rets_array, sizeof...(Rets));
+    // 调用函数
+    inter::Call(s, name, args_array, sizeof...(Args), rets_array, sizeof...(Rets));
 
-    // transfer vars to rets
-    inter::FakeluaFuncRetHelper(shared_from_this(), rets_array, rets);
+    // 将变量传输到返回值
+    inter::FakeluaFuncRetHelper(s, rets_array, rets);
 }
-
-// create fake_lua state
-FakeluaStatePtr FakeluaNewstate();
 
 }// namespace fakelua
