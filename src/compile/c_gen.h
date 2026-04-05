20b39c9cb2ac23b5bf9030e23f8cc16dc5b1b485
esrrhs <esrrhs@163.com>
2026-04-05 11:44:36 +0800

    Simplify CGen and remove unused function generation logic

--- src/compile/c_gen.h
+++ src/compile/c_gen.h
@@ -19,21 +19,10 @@ public:
 private:
     std::string Build(const CompileResult &cr, const CompileConfig &cfg);
     void GenerateHeader();
-    std::string GetFuncName(const SyntaxTreeInterfacePtr &ptr);
-    void GenerateDecl(const std::string &name, const SyntaxTreeInterfacePtr &body);
-    void GenerateFunction(const std::string &name, const SyntaxTreeInterfacePtr &body);
-    void CollectLocals(const SyntaxTreeInterfacePtr &node, std::set<std::string> &locals);
-    void GenerateBlock(const SyntaxTreeInterfacePtr &node, int indent);
-    void GenerateStmt(const SyntaxTreeInterfacePtr &stmt, int indent);
-    void GenerateExp(const SyntaxTreeInterfacePtr &node);
-    void GeneratePrefixexp(const SyntaxTreeInterfacePtr &node);
 
 private:
     State *s_;
     std::stringstream header_;
-    std::stringstream decl_;
-    std::stringstream impl_;
-    std::map<std::string, size_t> function_param_counts_;
 };
 
 }// namespace fakelua
