216d854cd72665f5eeb93dce7ec65eedc05fe547
esrrhs <esrrhs@163.com>
2026-06-21 16:20:23 +0800

    Refactor: Move inter multi-CVar functions to fakelua.cpp and strictly require VarType::Multi

--- src/var/var_multi.cpp
+++ src/var/var_multi.cpp
@@ -1,8 +1,6 @@
 #include "fakelua.h"
 #include "var/var_multi.h"
 #include "state/state.h"
-#include "var/var_type.h"
-#include "util/debug.h"
 
 namespace fakelua {
 
@@ -12,44 +10,4 @@ VarMulti *VarMulti::AllocTemp(State *state, uint32_t count) {
     return ret;
 }
 
-namespace inter {
-
-CVar AllocMultiCVar(State *s, int count) {
-    VarMulti *m = VarMulti::AllocTemp(s, count);
-    for (int i = 0; i < count; ++i) {
-        m->GetVars()[i] = CVar{static_cast<int>(VarType::Nil)};
-    }
-    CVar result;
-    result.type_ = static_cast<int>(VarType::Multi);
-    result.flag_ = 0;
-    result.data_.m = m;
-    return result;
-}
-
-void SetMultiCVarElement(CVar &multi, int idx, CVar val) {
-    DEBUG_ASSERT(multi.type_ == static_cast<int>(VarType::Multi));
-    VarMulti *m = multi.data_.m;
-    DEBUG_ASSERT(idx >= 0 && idx < static_cast<int>(m->GetCount()));
-    m->GetVars()[idx] = val;
-}
-
-CVar GetMultiCVarElement(const CVar &multi, int idx) {
-    if (multi.type_ != static_cast<int>(VarType::Multi)) {
-        return (idx == 0) ? multi : CVar{static_cast<int>(VarType::Nil)};
-    }
-    VarMulti *m = multi.data_.m;
-    if (idx < 0 || idx >= static_cast<int>(m->GetCount())) {
-        return CVar{static_cast<int>(VarType::Nil)};
-    }
-    return m->GetVars()[idx];
-}
-
-int GetMultiCVarCount(const CVar &multi) {
-    if (multi.type_ != static_cast<int>(VarType::Multi)) {
-        return 0;
-    }
-    return static_cast<int>(multi.data_.m->GetCount());
-}
-
-}// namespace inter
 }// namespace fakelua
