b5bc44458f060a2ce3b0a76dd3cee2993b00fb19
esrrhs <esrrhs@163.com>
2026-07-28 20:09:52 +0800

    test: split test_native_math_builtins into test_math_basic and test_math_trig cases

--- test/test_math.cpp
+++ test/test_math.cpp
@@ -4,7 +4,7 @@
 
 using namespace fakelua;
 
-TEST(test_math, test_native_math_builtins) {
+TEST(test_math, test_math_basic) {
     State *s = FakeluaNewState();
     ASSERT_NE(s, nullptr);
     CompileConfig config;
@@ -14,7 +14,17 @@ TEST(test_math, test_native_math_builtins) {
         double res1 = 0;
         Call(s, jit_type, "test_math_basic", res1);
         EXPECT_NEAR(res1, 157.14159265358979, 1e-4);
+    }
 
+    FakeluaDeleteState(s);
+}
+
+TEST(test_math, test_math_trig) {
+    State *s = FakeluaNewState();
+    ASSERT_NE(s, nullptr);
+    CompileConfig config;
+
+    for (auto jit_type: {JIT_TCC, JIT_GCC}) {
         CompileFile(s, "./math/test_math_trig.lua", config);
         double res2 = 0.0;
         Call(s, jit_type, "test_math_trig", res2);
